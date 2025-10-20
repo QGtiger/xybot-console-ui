aed4357adcb5d0144796fc4245de6fcf1b4c22be
光鱼 <guang.yu@yingdao.com>
2025-10-20 17:30:03 +0800

    feat: 更新版本号至 0.0.1-beta.29，重构 CustomModal 组件，新增样式文件

--- src/CustomModal/examples/demo1.tsx
+++ src/CustomModal/examples/demo1.tsx
@@ -1,65 +1,46 @@
-import { HomeOutlined } from '@ant-design/icons';
-import {
-  CustomModal,
-  Space,
-  ThemeModel,
-  UIButton,
-  UIInput,
-  UIModal,
-  useUIModal,
-} from '@xybot/ui';
+import { FormOutlined } from '@ant-design/icons';
+import { CustomModal, ThemeModel } from '@xybot/ui';
 
 export default () => {
   const { isDarkMode } = ThemeModel.useModel();
-  const { modal, modalHolder } = useUIModal();
 
   return (
     <div
       className="flex"
       style={{
-        display: 'flex',
-        gap: 16,
-        flexDirection: 'column',
+        // display: 'flex',
+        // gap: 16,
+        // flexDirection: 'column',
         padding: 16,
         background: isDarkMode ? '#202127' : '#f4f4f7',
-        alignItems: 'flex-start',
+        // alignItems: 'flex-start',
       }}
     >
-      <Space>
-        <UIButton
-          onClick={() => {
-            modal.warning({
-              title: '11',
-              content: <UIInput placeholder="请输入内容" />,
-              onCancel() {
-                console.log('cancel');
-              },
-            });
-          }}
-          icon={<HomeOutlined />}
-          type="border"
-        ></UIButton>
-        <UIButton
-          onClick={() => {
-            UIModal.error({
-              title: '11',
-              content: 'content',
-              cancelText: 'cancancancale',
-              onCancel() {
-                console.log('cancel');
-              },
-            });
-          }}
-          icon={<HomeOutlined />}
-          type="border"
+      <div
+        className=""
+        style={{
+          width: '100%',
+          background: '#fff',
+        }}
+      >
+        <CustomModal
+          title="测试"
+          subTitle="这是一个测试"
+          logo={<FormOutlined />}
+          extra={
+            <div
+              style={{
+                padding: '10px 20px',
+                background: isDarkMode ? '#2c2c34' : '#f9f9fb',
+              }}
+            >
+              额外引导内容
+            </div>
+          }
         >
-          {' '}
-          静态方法{' '}
-        </UIButton>
-
-        <CustomModal />
-        {modalHolder}
-      </Space>
+          主体内容
+        </CustomModal>
+      </div>
     </div>
   );
 };
--- src/CustomModal/index.tsx
+++ src/CustomModal/index.tsx
@@ -1,3 +1,66 @@
-export function CustomModal() {
-  return <div>CustomModal</div>;
+import React, { PropsWithChildren } from 'react';
+
+import { useBoolean } from 'ahooks';
+import './index.less';
+
+export interface CustomModalProps {
+  title: React.ReactNode;
+  subTitle?:
+    | React.ReactNode
+    | ((toggleNode: React.ReactNode) => React.ReactNode);
+  logo?: React.ReactNode;
+  // extra
+  extra?: React.ReactNode;
+}
+
+export function CustomModal(props: PropsWithChildren<CustomModalProps>) {
+  const { title, subTitle, logo, extra, children } = props;
+  const [showExtra, showExtraAction] = useBoolean(false);
+
+  const toggleNode = extra && (
+    <div className="extra-toggle" onClick={() => showExtraAction.toggle()}>
+      <span>{showExtra ? '收起' : '展开'}引导</span>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="12"
+        height="12"
+        viewBox="0 0 12 12"
+        fill="none"
+      >
+        <path
+          d="M9 7.5L6 4.5L3 7.5"
+          stroke="currentColor"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </svg>
+    </div>
+  );
+
+  const renderSubTitle = () => {
+    if (!subTitle) return null;
+    if (typeof subTitle === 'function') {
+      return subTitle(toggleNode);
+    }
+    return (
+      <>
+        <div className="sub-title">{subTitle}</div>
+        {toggleNode}
+      </>
+    );
+  };
+
+  return (
+    <div className="ui-custom-modal">
+      <div className="ui-custom-modal-header">
+        <div className="logo">{logo}</div>
+        <div className="title-wrapper">
+          <div className="title">{title}</div>
+          <div className="sub">{renderSubTitle()}</div>
+        </div>
+      </div>
+      {showExtra && <div className="extra-content">{extra}</div>}
+      <div className="ui-custom-modal-content">{children}</div>
+    </div>
+  );
 }
