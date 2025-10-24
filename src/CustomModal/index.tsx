39a271d0e6b18adcd6ca44013fcca6cc46136970
光鱼 <guang.yu@yingdao.com>
2025-10-24 14:03:40 +0800

    feat: 优化 CustomModalContent 组件的 toggleNode 显示内容，增强用户体验

--- src/CustomModal/index.tsx
+++ src/CustomModal/index.tsx
@@ -73,21 +73,27 @@ function CustomModalContent(
 
   const toggleNode = extra && (
     <div className="extra-toggle" onClick={() => showExtraAction.toggle()}>
-      <span>{showExtra ? '收起' : '展开'}引导</span>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="12"
-        height="12"
-        viewBox="0 0 12 12"
-        fill="none"
-      >
-        <path
-          d="M9 7.5L6 4.5L3 7.5"
-          stroke="currentColor"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        />
-      </svg>
+      {showExtra ? (
+        <>
+          收起引导
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="12"
+            height="12"
+            viewBox="0 0 12 12"
+            fill="none"
+          >
+            <path
+              d="M9 7.5L6 4.5L3 7.5"
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+        </>
+      ) : (
+        <>如何操作?</>
+      )}
     </div>
   );
 
